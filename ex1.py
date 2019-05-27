0fc6c46d5d8428d9bb5d7dd05502e34551ba526f
ZhangYajun12138 <18521064458@163.com>
2019-05-27 11:13:30 +0800

    习题8 打印

--- ex1.py
+++ ex1.py
@@ -67,3 +67,14 @@ end11 = "e"
 end12 = "r"
 print(end1 + end2 + end3 + end4 + end5 + end6)
 print(end7 + end8 + end9 + end10 + end11 + end12)
+
+#xiti 8
+formatter = "%r %r %r %r"
+print(formatter %(1,2,3,4))
+print(formatter %('one','two','three','four'))
+print(formatter %(True,False,False,True))
+print(formatter %(formatter,formatter,formatter,formatter))
+print(formatter %('I had this thing',
+                  'That you could type up right.',
+                  "But it didn't sing.",
+                  "So I said goodnight."))
