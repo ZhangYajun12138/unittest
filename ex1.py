e6e2f778222e2bcb3507dc0a28cd08aeed33ad06
ZhangYajun12138 <18521064458@163.com>
2019-05-27 11:27:25 +0800

    习题9 打印

--- ex1.py
+++ ex1.py
@@ -1,8 +1,11 @@
 #coding:utf-8
+
+#习题1
 # print('Hello World!',end='')
 # print('Hello Again')
 # print('Hens',25+30/6)
 
+#习题2
 # cars = 100
 # space_in_a_car = 4
 # drivers = 30
@@ -18,6 +21,7 @@
 # print('We have',passengers,'to carpool today.')
 # print('We need to put about',average_passengers_per_car,'in each car.')
 
+#习题5
 # my_name = 'Zed A. Shaw'
 # my_age = 35 # not a lie
 # my_height = 74
@@ -33,6 +37,7 @@
 # print("His teeth are usually %s depending on the coffee." % my_teeth)
 # print("If I add %d,%d,and %d I get %d." % (my_age,my_height,my_weight,my_age + my_height + my_weight))
 
+#习题6
 # x = "There are %d types of people." % 10 # 10种人
 # binary = "binary"
 # do_not = "don't"
@@ -48,33 +53,46 @@
 # e = "a string with a right side."
 # print(w + e)
 
-#xiti 7
-print("Mary had a little lamb.")
-print("Its fleece was white as %s." % 'show' )
-print("And everyone that Mary went.")
-print("." * 10) # what'd that do?
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
-print(end1 + end2 + end3 + end4 + end5 + end6)
-print(end7 + end8 + end9 + end10 + end11 + end12)
+#习题7
+# print("Mary had a little lamb.")
+# print("Its fleece was white as %s." % 'show' )
+# print("And everyone that Mary went.")
+# print("." * 10) # what'd that do?
+# end1 = "C"
+# end2 = "h"
+# end3 = "e"
+# end4 = "e"
+# end5 = "s"
+# end6 = "e"
+# end7 = "B"
+# end8 = "u"
+# end9 = "r"
+# end10 = "g"
+# end11 = "e"
+# end12 = "r"
+# print(end1 + end2 + end3 + end4 + end5 + end6)
+# print(end7 + end8 + end9 + end10 + end11 + end12)
 
-#xiti 8
-formatter = "%r %r %r %r"
-print(formatter %(1,2,3,4))
-print(formatter %('one','two','three','four'))
-print(formatter %(True,False,False,True))
-print(formatter %(formatter,formatter,formatter,formatter))
-print(formatter %('I had this thing',
-                  'That you could type up right.',
-                  "But it didn't sing.",
-                  "So I said goodnight."))
+#习题8
+# formatter = "%r %r %r %r"
+# print(formatter %(1,2,3,4))
+# print(formatter %('one','two','three','four'))
+# print(formatter %(True,False,False,True))
+# print(formatter %(formatter,formatter,formatter,formatter))
+# print(formatter %('I had this thing',
+#                   'That you could type up right.',
+#                   "But it didn't sing.",
+#                   "So I said goodnight."))
+
+# 习题9
+# Here's some new strange stuff,remember type it exactly.
+days = "Mon Tue Wen Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nJun\nJul\nAug"
+print("Here are the days:",days)
+print("Here are the months:",months)
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want,or 5,or 6.
+""")
