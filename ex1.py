dc260025900222323130f3958451b9156011583b
ZhangYajun12138 <18521064458@163.com>
2019-05-27 11:42:05 +0800

    习题10 转义字符

--- ex1.py
+++ ex1.py
@@ -86,13 +86,30 @@
 
 # 习题9
 # Here's some new strange stuff,remember type it exactly.
-days = "Mon Tue Wen Thu Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nJun\nJul\nAug"
-print("Here are the days:",days)
-print("Here are the months:",months)
-print("""
-There's something going on here.
-With the three double-quotes.
-We'll be able to type as much as we like.
-Even 4 lines if we want,or 5,or 6.
-""")
+# days = "Mon Tue Wen Thu Fri Sat Sun"
+# months = "Jan\nFeb\nMar\nApr\nJun\nJul\nAug"
+# print("Here are the days:",days)
+# print("Here are the months:",months)
+# print("""
+# There's something going on here.
+# With the three double-quotes.
+# We'll be able to type as much as we like.
+# Even 4 lines if we want,or 5,or 6.
+# """)
+
+# 习题10
+print("I am 6'2\" tall.")
+print('I am 6\'2" tall.')
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
