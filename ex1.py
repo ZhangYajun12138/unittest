2dac3649ed7bb807ecec0a69f472c4c4434f4dbb
ZhangYajun12138 <18521064458@163.com>
2019-05-27 10:51:01 +0800

    习题6，字符串和文本

--- ex1.py
+++ ex1.py
@@ -18,17 +18,33 @@
 # print('We have',passengers,'to carpool today.')
 # print('We need to put about',average_passengers_per_car,'in each car.')
 
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74
-my_weight = 180
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Black'
-print("Let's talk about %r." % my_name)
-print("He's %r inches tall." % (my_height * 2))
-print("He's %d pounds heavy." % my_weight)
-print("Actually that's not too heavy.")
-print("He's got %s eyes and %s hair." %(my_eyes,my_hair))
-print("His teeth are usually %s depending on the coffee." % my_teeth)
-print("If I add %d,%d,and %d I get %d." % (my_age,my_height,my_weight,my_age + my_height + my_weight))
+# my_name = 'Zed A. Shaw'
+# my_age = 35 # not a lie
+# my_height = 74
+# my_weight = 180
+# my_eyes = 'Blue'
+# my_teeth = 'White'
+# my_hair = 'Black'
+# print("Let's talk about %r." % my_name)
+# print("He's %r inches tall." % (my_height * 2))
+# print("He's %d pounds heavy." % my_weight)
+# print("Actually that's not too heavy.")
+# print("He's got %s eyes and %s hair." %(my_eyes,my_hair))
+# print("His teeth are usually %s depending on the coffee." % my_teeth)
+# print("If I add %d,%d,and %d I get %d." % (my_age,my_height,my_weight,my_age + my_height + my_weight))
+
+# x = "There are %d types of people." % 10 # 10种人
+# binary = "binary"
+# do_not = "don't"
+# y = "Those who know %s and those who %s." %(binary,do_not)
+# print(x)
+# print(y)
+# print("I said: %r." % x)
+# print("I also said: '%s'." % y)
+# hilarious = False
+# joke_evaluation = "Isn't that joke so funny? %r"
+# print(joke_evaluation % hilarious)
+# w = "This is the left side of..."
+# e = "a string with a right side."
+# print(w + e)
+
