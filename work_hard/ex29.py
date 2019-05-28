3225c278ff3d66b554390c9cbb91f9372ec6d74b
ZhangYajun12138 <18521064458@163.com>
2019-05-28 13:46:14 +0800

    28.bool 29.if

---
+++ work_hard/ex29.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+# if
+people = 20
+cats = 30
+dogs = 15
+if people < cats:
+    print("people < cats")
+elif people > cats:
+    print("people > cats")
+else:
+    print("people = cats")
